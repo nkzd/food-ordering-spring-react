2c42ec8cf2afebd61a1737e7e879fbd1d510e06a
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2020-01-25 16:08:12 +0100

    Add production build

--- food-ordering-frontend/src/index.js
+++
@@ -1,4 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import App from "./App";
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
--- food-ordering-frontend/src/pages/admin/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-  return <p>Whatever yo</p>;
-};
-
-export default NotFound;
